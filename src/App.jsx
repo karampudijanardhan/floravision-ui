e1185a54ae8750083c8d9722a69838c7f327de72
karampudijanardhan <janardhankarampudi@gmail.com>
2026-05-11 15:02:05 +0530

    Added app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import CustomerReviews from "./sections/CustomerReviews";
+import O2Section from "./sections/O2Section";
+import TopSellingplants from "./sections/TopSellingPlants";
 
 function App() {
   return (
     <div>
       <Navbar />
-
+      <HeroSection />
+      <TopSellingplants />
+      <CustomerReviews />
+      <O2Section />
+      <Footer/>
     </div>
   );
 }
